8503e654766329735a1a993986a9c54e25687607
fabimaria <147111232+fabimaria@users.noreply.github.com>
2024-07-03 21:39:58 -0300

    Laços_de_repetição_03.07.2024

---
+++ Python Fabi/lacosderepeticao.py
@@ -0,0 +1,15 @@
+#x = 0
+
+#while x < 5:
+    #nome = input ('Qual é o seu nome?  ')
+    #x=x+1
+
+x = 0
+animais = []
+
+while x < 5:
+    nome = input ('Qual é o seu nome?  ')
+    animais.append(nome)
+    x=x+1
+
+    print(animais)
